94889307d91879f085fe70bc4109b63d22322b9f
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-10-28 10:38:22 +0900

    メソッドの更新中

--- Practice03/src/GameCharacter.java
+++ Practice03/src/GameCharacter.java
@@ -1,14 +1,38 @@
 
 public class GameCharacter {
 	//カプセル化したフィールド
-	String nameString; //キャラクターの名前
+	String name; //キャラクターの名前
 	int health; //体力
-	int currenthealth; //戦闘中のリアルタイム体力
+	int currentHealth; //戦闘中のリアルタイム体力
 	int attack; //攻撃力
-	int defencse; //防御力
-	
+	int defense; //防御力
+
 	//コンストラクタ
-	public GameCharacter(String name,int health,int attack,int defense) {
+	public GameCharacter(String name, int health, int attack, int defense) {
+		this.name = name;
+		this.health = health;
+		currentHealth = this.health;
+		this.attack = attack;
+		this.defense = defense;
+	}
+
+	//	ステータス表示メソッド
+	public void DisplayStatus() {
+		System.out.println("【"+ name + "】");
+		System.out.print("HP:" + health + "/");
+		System.out.print("攻撃力:" + attack + "/");
+		System.out.println("防御力:" + defense);
+	}
+	
+	//攻撃メソッド
+	public void Attack(GameCharacter target) {
+		System.out.print(name + "の攻撃！ -->");
+		target.TakeDamage(attack); //相手は変数attackの力でダメージを負う
+	}
+	
+	//被ダメージメソッド
+	public void TakeDamage(int attack) {
 		
 	}
+
 }
