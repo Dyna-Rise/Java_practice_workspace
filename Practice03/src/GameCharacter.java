72532a728111ac3b0a3915a251762de2e265ff8c
DESKTOP-C5T1MTL\user <dynarise.headquarters@gmail.com>
2025-10-28 10:13:06 +0900

    Practice03とGameCharacter.javaの作成

---
+++ Practice03/src/GameCharacter.java
@@ -0,0 +1,14 @@
+
+public class GameCharacter {
+	//カプセル化したフィールド
+	String nameString; //キャラクターの名前
+	int health; //体力
+	int currenthealth; //戦闘中のリアルタイム体力
+	int attack; //攻撃力
+	int defencse; //防御力
+	
+	//コンストラクタ
+	public GameCharacter(String name,int health,int attack,int defense) {
+		
+	}
+}
